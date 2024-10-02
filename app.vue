8a9d59ee8b669939cdfaf2df0358ae4758a4e438
Goldenback <iliesrimani.work@gmail.com>
2024-10-02 16:59:10 +0200

    feat: added directories

--- app.vue
+++ app.vue
@@ -1,6 +1,40 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <NuxtLayout>
+        <NuxtPage/>
+    </NuxtLayout>
 </template>
+
+<script setup>
+/*
+const SEO = useSeoMeta({
+    title: "",
+    description: "",
+    ogTitle: "",
+    ogDescription: "",
+    ogImage: "",
+    ogUrl: "",
+    ogType: "",
+    twitterCard: "",
+    twitterSite: "",
+    twitterTitle: "",
+    twitterDescription: "",
+    twitterImage: "",
+    icon: "",
+    canonical: "",
+    robots: "",
+    lang: "",
+});
+*/
+</script>
+
+<style>
+:root {
+    --custom-color: #333;
+}
+
+body {
+    background-color: var(--custom-color);
+    color: white;
+    font-family: 'Arial', sans-serif;
+}
+</style>
