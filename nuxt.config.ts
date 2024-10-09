ee7d4ee28785c8830760b221fd10c1811b291a4e
thierrymgn <maignan.thierry@outlook.fr>
2024-10-09 10:01:53 +0200

    feat: add @nuxtjs/sitemap module and update site configuration
    This commit adds the @nuxtjs/sitemap module to the project and updates the site configuration in the nuxt.config.ts file. The module is added as a dependency in the package-lock.json and package.json files. The site configuration now includes the URL and name of the site.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2024-04-03',
-    devtools: {enabled: true}
+  compatibilityDate: '2024-04-03',
+  devtools: {enabled: true},
+  modules: ['@nuxtjs/sitemap'],
+  site: {
+    url: 'https://crakilouf.me/',
+    name: 'Crakilouf : Découvrez la Vérité sur cette Créature Mythique'
+  },
 })
