703c1f78cd0d3ee9b77d82935587d1aa2a82dfd2
Goldenback <iliesrimani.work@gmail.com>
2024-10-09 12:00:49 +0200

    lib: added nuxt-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: {enabled: true},
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
 
-  modules: ['@nuxtjs/sitemap', 'nuxt-gtag'],
+    modules: ['@nuxtjs/sitemap', 'nuxt-gtag', '@nuxt/ui'],
 
-  site: {
-    url: 'https://crakilouf.me/',
-    name: 'Crakilouf : Découvrez la Vérité sur cette Créature Mythique'
-  },
+    site: {
+        url: 'https://crakilouf.me/',
+        name: 'Crakilouf : Découvrez la Vérité sur cette Créature Mythique'
+    },
 
-  app: {
-    head: {
-      meta: [
-        { name: 'google-site-verification', content: 'uS7naheZPgCr-IL11X46MQtn1-Dsn6fFpegxgHgx4dk' }
-      ]
+    app: {
+        head: {
+            meta: [
+                {name: 'google-site-verification', content: 'uS7naheZPgCr-IL11X46MQtn1-Dsn6fFpegxgHgx4dk'}
+            ]
+        }
+    },
+
+    gtag: {
+        id: 'G-RS40GBHHL4'
     }
-  },
-
-  gtag: {
-    id: 'G-RS40GBHHL4'
-  }
 })
