a4a7e13ff24f082919224d96412c49eb9941be52
thierrymgn <maignan.thierry@outlook.fr>
2024-10-09 10:43:57 +0200

    refactor: update SEO metadata and content on app and index pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,11 @@ export default defineNuxtConfig({
     url: 'https://crakilouf.me/',
     name: 'Crakilouf : Découvrez la Vérité sur cette Créature Mythique'
   },
+  app: {
+    head: {
+      meta: [
+        { name: 'google-site-verification', content: 'uS7naheZPgCr-IL11X46MQtn1-Dsn6fFpegxgHgx4dk' }
+      ],
+    }
+  }
 })
